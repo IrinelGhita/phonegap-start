7d00ab0a4152561af8f3fa9cd8f6dafb2c3fbf2c
Costinel I. Ghita <ghic1_15@uni.worc.ac.uk>
2016-10-04 10:58:37 +0100

    Test 2
    22

--- www/js/device_details.js
+++ www/js/device_details.js
@@ -1,12 +1,20 @@
+ var log = document.getElementById("log");
+
+ log.innerHTML += "Log created <br />";
+
 // Cordova is loading.....
 document.addEventListener("deviceready", onDeviceReady, false);
 // Now the Cordova is ready       
-function onDeviceReady()
-    {
+function onDeviceReady() {
+    log.innerHTML += "Device ready <br />";
+
     var element = document.getElementById('device_details');
-    element.innerHTML = 'Device Name: ' + device.name + '<br />' +'<br/>'+
-                        'Device Cordova: ' + device.cordova + '<br />' + '<br/>' +
-                        'Device Platform: ' + device.platform + '<br />' + '<br/>' +
-                        'Device UUID: ' + device.uuid + '<br />' + '<br/>' +
-                        'Device Version: ' + device.version + '<br />' + '<br/>';
-    } 
+
+    element.innerHTML = 'Device Name: '     + device.name       + '<br />' +'<br/>'     +
+                        'Device Cordova: '  + device.cordova    + '<br />' + '<br/>'    +
+                        'Device Platform: ' + device.platform   + '<br />' + '<br/>'    +
+                        'Device UUID: '     + device.uuid       + '<br />' + '<br/>'    +
+                        'Device Version: '  + device.version    + '<br />' + '<br/>';
+
+    log.innerHTML += "Device details updated <br />";
+}
