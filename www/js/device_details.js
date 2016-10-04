ce55ed3346ca740f1a0ef015e8851d091d49c4c2
Costinel I. Ghita <ghic1_15@uni.worc.ac.uk>
2016-10-04 10:50:08 +0100

    It should work
    Tesst1 1

--- www/js/device_details.js
+++ www/js/device_details.js
@@ -1,70 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        switch (id) {
-            case "deviceready":
-                showDeviceDetails();
-                break;
-        }
-    },
-
-    showDeviceDetails: function() {
-        var deviceDetails = getDeviceDetails();
-
-        var deviceDetails = document.getElementById("device_details");
-
-        var deviceName      = document.getElementById("device_name");
-        var devicePlatform  = document.getElementById("device_platform");
-        var deviceVersion   = document.getElementById("device_version");
-        var deviceReady     = document.getElementById("device_ready");
-
-        deviceName.innerHTML        = deviceDetails.device_name;
-        devicePlatform.innerHTML    = deviceDetails.device_platform;
-        deviceVersion.innerHTML     = deviceDetails.device_version;
-        deviceReady.innerHTML       = "Device is ready!";
-    },
-
-    getDeviceDetails: function() {
-        return {
-            device_name         : device.name,
-            device_platform     : device.platform,
-            device_version      : device.version
-        };
-    },
-};
+// Cordova is loading.....
+document.addEventListener("deviceready", onDeviceReady, false);
+// Now the Cordova is ready       
+function onDeviceReady()
+    {
+    var element = document.getElementById('device_details');
+    element.innerHTML = 'Device Name: ' + device.name + '<br />' +'<br/>'+
+                        'Device Cordova: ' + device.cordova + '<br />' + '<br/>' +
+                        'Device Platform: ' + device.platform + '<br />' + '<br/>' +
+                        'Device UUID: ' + device.uuid + '<br />' + '<br/>' +
+                        'Device Version: ' + device.version + '<br />' + '<br/>';
+    } 
